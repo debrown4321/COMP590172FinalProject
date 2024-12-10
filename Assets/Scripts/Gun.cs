f76e1cb9c4326cbce0c78b76a455cf0ac8a338a2
Davis Brown <debrown@unc.edu>
2024-12-10 16:14:42 -0500

    Rope and disappearing bullets added

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Oculus.Interaction;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -9,6 +10,7 @@ public class Gun : MonoBehaviour
     [Header("Bullet Info")]
     public GameObject bulletGO;
     public float bulletSpeed;
+    public float reloadSpeed;
     Transform bulletTransform;
     Rigidbody bulletRb;
     Bullet bulletScript;
@@ -16,34 +18,48 @@ public class Gun : MonoBehaviour
     [Header("Player Info")]
     public GameObject player;
     SpringJoint springJoint;
-    [Range(500f, 2000f)]
-    public float damper = 500f;
-    [Range(1000f, 3000f)]
-    public float spring = 1000f;
+    [Range(1f, 20f)]
+    public float damper = 7f;
+    [Range(1f, 20f)]
+    public float spring = 4.5f;
+    [Range(1f, 20f)]
+    public float massScale = 4.5f;
 
     [Header("Gun Info")]
     public Transform barrel;
-    public bool Shot { get; set; }
+    public LineRenderer lr;
+    public bool Shooting { get; set; }
+    public bool Reloading { get; set; }
 
     private void Start()
     {
         bulletTransform = bulletGO.transform;
         bulletRb = bulletGO.GetComponent<Rigidbody>();
         bulletScript = bulletGO.GetComponent<Bullet>();
+        lr = gameObject.GetComponent<LineRenderer>();
+        Shooting = false;
+        Reloading = false;
+        lr.positionCount = 2;
     }
 
     public void Fire()
     {
-        Shot = true;
+        lr.positionCount = 2;
+        bulletGO.SetActive(true);
+        Shooting = true;
         bulletTransform.position = barrel.position;
         bulletRb.velocity = barrel.forward * bulletSpeed;
+        
     }
 
     public void Reload()
     {
-        Shot = false;
+        Shooting = false;
+        Reloading = true;
         Destroy(springJoint);
         bulletScript.DestroyJoint();
+        lr.positionCount = 0;
+
     }
 
     public void Swing()
@@ -61,15 +77,38 @@ public class Gun : MonoBehaviour
 
         springJoint.damper = damper;
         springJoint.spring = spring;
+        springJoint.massScale = massScale;
+
+        
+    }
+
+    void DrawRope()
+    {
+        if (Shooting)
+        {
+            lr.SetPosition(0, barrel.position);
+            lr.SetPosition(1, bulletTransform.position);
+        }
     }
 
     private void Update()
     {
-        if (!Shot)
+
+        if (!Shooting && !Reloading)
         {
+            bulletGO.SetActive(false);
             bulletTransform.position = barrel.position;
             bulletTransform.forward = barrel.forward;
+        } 
+        else if (Reloading)
+        {
+            bulletTransform.position = Vector3.MoveTowards(bulletTransform.position, barrel.position, reloadSpeed);
         }
+    }
+
+    private void LateUpdate()
+    {
+        DrawRope();
     }
 
 }
